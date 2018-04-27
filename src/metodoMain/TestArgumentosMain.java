f89ddc5c86d83077580dd668fe53c7d8598b8285
manuelrojano <manuelrojano19@hotmail.com>
2018-04-27 12:55:59 +0200

    prueba

---
+++ src/metodoMain/TestArgumentosMain.java
@@ -0,0 +1,16 @@
+package metodoMain;
+
+public class TestArgumentosMain {
+
+	public static void main(String[] args) {
+		
+		System.out.println("El numero de argumentos pasados son: " + args.length);
+		System.out.print("Mostrando los argumentos pasados al reves y en mayuscula: ");
+		for(int i=args.length-1; i>-1; i--) {
+			System.out.print(args[i].toUpperCase());
+			System.out.print(" ");
+		}
+
+	}
+
+}
